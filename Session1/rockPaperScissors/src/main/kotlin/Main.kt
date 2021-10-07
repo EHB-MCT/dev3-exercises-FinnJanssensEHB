6ce3e330ef2c993497134c0de505760f5cabd45a
FinnJanssensEHB <finn.janssens@student.ehb.be>
2021-10-07 16:47:02 +0200

    S1E2.1 Spock and Lizard, scoring and input validation

--- Session1/rockPaperScissors/src/main/kotlin/Main.kt
+++ Session1/rockPaperScissors/src/main/kotlin/Main.kt
@@ -1,39 +1,73 @@
 fun main() {
-    getWinner(getUserInput(), getComputerChoice())
+    var scoreUser = 0
+    while (true) {
+        scoreUser += getWinner(getUserInput(), getComputerChoice())
+        println("SCORE: $scoreUser")
+    }
 }
 
 fun getUserInput(): String? {
-    println("Type rock, paper or scissors")
+    println("Type rock, paper, scissors, spock or lizard")
     val choice = readLine()
-    return choice
+    if (choice == "rock" || choice == "paper" || choice == "scissors" || choice == "spock" || choice == "lizard") {
+        return choice
+    } else {
+        println("Input not allowed")
+        return null
+    }
 }
 
-fun getWinner(userInput: String?, computerInput: String?) {
+fun getWinner(userInput: String?, computerInput: String?): Int {
     if (userInput == computerInput) {
         println("Tie!")
+        return 0
     } else if (userInput == "rock") {
-        if (computerInput == "paper") {
+        if (computerInput == "paper" || computerInput == "spock") {
             println("You lose!")
+            return 0
         } else {
             println("You win!")
+            return 1
         }
     } else if (userInput == "paper") {
-        if (computerInput == "scissors") {
+        if (computerInput == "scissors"  || computerInput == "lizard") {
             println("You lose!")
+            return 0
         } else {
             println("You win!")
+            return 1
         }
     } else if (userInput == "scissors") {
-        if (computerInput == "paper") {
+        if (computerInput == "rock" || computerInput == "spock") {
             println("You lose!")
+            return 0
         } else {
             println("You win!")
+            return 1
         }
+    } else if (userInput == "spock") {
+        if (computerInput == "paper" || computerInput == "lizard") {
+            println("You lose!")
+            return 0
+        } else {
+            println("You win!")
+            return 1
+        }
+    } else if (userInput == "lizard") {
+        if (computerInput == "scissors"  || computerInput == "rock") {
+            println("You lose!")
+            return 0
+        } else {
+            println("You win!")
+            return 1
+        }
+    } else {
+        return 0
     }
 }
 
 fun getComputerChoice(): String? {
-    val options = arrayOf("rock", "paper", "scissors")
+    val options = arrayOf("rock", "paper", "scissors", "spock", "lizard")
     val computerChoice = options.random()
     println("Computer chose $computerChoice")
     return computerChoice
