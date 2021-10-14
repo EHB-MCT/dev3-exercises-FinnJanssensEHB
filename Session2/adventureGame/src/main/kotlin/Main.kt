0632fb74f6ac9ab5ac994841993959fa543267f5
FinnJanssensEHB <finn.janssens@student.ehb.be>
2021-10-14 15:28:17 +0200

    Wrote intro text

---
+++ Session2/adventureGame/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var playerEnergy = 10
+
+    println("Welcome! You are on a deserted island. Your goal is to survive as long as possible on this island.")
+    println("Challenges will make you gain or lose energy.")
+    println("Your current energy is $playerEnergy")
+    println("When your energy is at 0, the game is over.")
+    println("Good luck.")
+
+}
