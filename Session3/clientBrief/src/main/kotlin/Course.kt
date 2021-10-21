0e7f16a50883930db48f05d9dbbb3230a824fc52
FinnJanssensEHB <finn.janssens@student.ehb.be>
2021-10-21 15:19:29 +0200

    S3E1: Added Course class

---
+++ Session3/clientBrief/src/main/kotlin/Course.kt
@@ -0,0 +1,10 @@
+class Course(
+    val name: String,
+    val description: String,
+    val year: int,
+    val field: String,
+    val cluster: String,
+    val semester: Int,
+    val credits: Int
+) {
+}
