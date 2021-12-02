8dd28d9346c5889df3e6f8a3bca8f23904da1fbe
FinnJanssensEHB <finn.janssens@student.ehb.be>
2021-12-02 17:36:01 +0100

    Add human delay and hinting functionallity

--- Session7/Duolingo-2/src/main/kotlin/Duolingo.kt
+++ Session7/Duolingo-2/src/main/kotlin/Duolingo.kt
@@ -1,3 +1,5 @@
+import kotlin.concurrent.thread
+
 class Duolingo {
     var roundSize: Int = 3
     var language: String = "engels"
@@ -53,18 +55,31 @@ class Duolingo {
         val selectedWords = selectRandomWords(wordSet)
         var i = selectedWords.size
         for (word in selectedWords) {
+            Thread.sleep(400)
             println("Vertaal ${word.original} van het ${word.language} naar het Nederlands!")
             println("Nog ${i} woorden te gaan!")
-            val userInput = readLine()
-            if (userInput == word.translated) {
-                println("Correct!")
-            } else {
-                println("Fout!")
-            }
+            validateResponse(word)
             i--
         }
     }
     fun selectRandomWords(wordSet: MutableSet<Word>): MutableSet<Word> {
         return wordSet.shuffled().take(roundSize).toMutableSet()
+    }
+
+    fun validateResponse(word: Word) {
+        val userInput = readLine()
+        if (userInput == word.translated) {
+            println("Thinking...")
+            Thread.sleep(100)
+            println("Correct!")
+        } else if (userInput == "HINT") {
+            println("The first letter is ${word.translated.first()}")
+            validateResponse(word)
+        }
+        else {
+            println("Thinking...")
+            Thread.sleep(100)
+            println("Fout!")
+        }
     }
 }
